f3c9341f60e458536097fc6a4e2e77fab5bdc584
Bùi Minh Tuấn <buiminhtuan044@gmail.com>
2025-07-01 21:29:54 +0700

    Update user.ts

--- types/user.ts
+++ types/user.ts
@@ -1,1 +1,13 @@
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: 'ADMIN' | 'MODERATOR' | 'THERAPIST' | 'COACH' | 'PREMIUM_MEMBER' | 'MEMBER';
+  isActive: boolean;
+  emailVerified: boolean;
+  createdAt: string;
+  lastLoginAt?: string;
+}
 
