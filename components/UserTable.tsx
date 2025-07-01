461060d231756f7799e70075b623d3e7b2834daf
Bùi Minh Tuấn <buiminhtuan044@gmail.com>
2025-07-01 21:43:26 +0700

    Update UserTable.tsx

--- components/UserTable.tsx
+++ components/UserTable.tsx
@@ -1,1 +1,23 @@
+import React from 'react';
+import { UserCheck, Clock, AlertCircle } from 'lucide-react';
+import { User } from '../types/user';
+
+interface UserTableProps {
+  users: User[];
+}
+
+const UserTable: React.FC<UserTableProps> = ({ users }) => (
+  <div className="bg-white rounded-lg shadow-md overflow-hidden">
+    {/* ...phần header giống code gốc... */}
+    <tbody>
+      {users.map(user => (
+        <tr key={user.id} className="hover:bg-gray-50">
+          {/* ... */}
+        </tr>
+      ))}
+    </tbody>
+  </div>
+);
+
+export default UserTable;
 
